868ef11152fc4b5e029e370a11151e24f8a4db6b
Huong <admin@Apples-MacBook-Pro.local>
2018-07-16 20:08:54 +0700

    quản lý đơn hàng

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,10 +4,35 @@
 <div class="panel panel-body">
     <section class="content">
         <div class="panel panel-default">
-            <div class="panel-heading"><b>Trang chủ</b>
+            <div class="panel-heading"><b>Quản lý đơn hàng mới (Đơn hàng đã xác nhận) </b>
             </div>
             <div class="panel-body">
-                content
+                <table class="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th>Tên khách hàng</th>
+                        <th>Lastname</th>
+                        <th>Email</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>john@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>Mary</td>
+                        <td>Moe</td>
+                        <td>mary@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>July</td>
+                        <td>Dooley</td>
+                        <td>july@example.com</td>
+                      </tr>
+                    </tbody>
+                  </table>
             </div>
         </div>
     </section>
